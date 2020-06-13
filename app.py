d70fbd5113a73603405fc8e2cae708060ac4c68b
akuby21 <39649594+akuby21@users.noreply.github.com>
2020-06-14 00:11:51 +0900

    단어 분석함수 수정 및 실행시간 측정 기능 추가

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import requests
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
 from elasticsearch import Elasticsearch
+import time
 
 app = Flask(__name__)
 
@@ -17,55 +18,33 @@ def index():
 @app.route('/analyze', methods=['GET'])
 def analyze():
 	if request.method == 'GET':
+		start = time.time()
 		URL = request.args.get('url')
 		w = s(URL)
-		return render_template('analyze.html', word = w)
+		end = time.time()
+		t = round(end -start,2)
+		return render_template('analyze.html', url=URL,word_num = len(w),time = t)
 
 def s(URL):
-	res = requests.get(URL)
-	html = BeautifulSoup(res.content, "html.parser")
-	all_link = html.find_all("p")
-	list_word = []
-	word_counter = []
-	size = 1
+   frequency = {}
+   splits = []
+   res = requests.get(URL)
+   html = BeautifulSoup(res.content, "html.parser")
+   all_link = html.find_all("p")
 
-	for link in all_link: 
-		word = link.text.split(' ')	
-		for i in range(0,len(word)):
-			word[i] = word[i].replace(",","")
-			word[i] = word[i].replace(".","")
-			word[i] = word[i].replace("\n","")
-			word[i] = word[i].replace("!","")
-			word[i] = word[i].replace("?","")
-			word[i] = word[i].replace("(","")
-			word[i] = word[i].replace(")","")
-			word[i] = word[i].replace("[","")
-			word[i] = word[i].replace("]","")
-			word[i] = word[i].replace("{","")
-			word[i] = word[i].replace("}","")
-			word[i] = word[i].replace("\"","")
-			word[i] = word[i].replace("'","")
-			word[i] = word[i].replace("<","")
-			word[i] = word[i].replace(">","")
-			word[i] = word[i].replace(";","")
-			word[i] = word[i].replace("-","")
-			word[i] = word[i].replace("~","")
-			word[i] = word[i].replace("`","")
-			word[i] = word[i].replace(":","")
-			word[i] = word[i].replace(";","")
-			word[i] = word[i].lower()	
-			judge = -1
-			for j in range(0,len(list_word)):
-				if(word[i] == list_word[j]):
-					judge = j
-					break
-			if(judge == -1):
-				list_word.append(word[i])
-				word_counter.append(1)	
-			else:
-				word_counter[judge] += 1
+   for t in all_link:
+      splits = re.sub('<sup.*?>.*?</sup>','',str(t),0)
+      splits = re.sub('<.+?>','',splits,0)
+      splits = re.sub('[-=+,#/\?:^$.@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]','',splits).lower().split()
+      for i in splits:
+         try:
+            cnt = frequency[i]
+            frequency[i] = cnt + 1
+         except KeyError:
+            frequency[i] = 1
 
-	return list_word
+   frequency = list(sorted(frequency.items(),key=operator.itemgetter(1),reverse = True))
+   return frequency
 
 if __name__ == '__main__':
     app.run(debug=False,host ="127.0.0.1",port ="5000")
